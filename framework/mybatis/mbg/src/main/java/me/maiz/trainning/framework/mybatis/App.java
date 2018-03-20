80f2a48304b217f889cc5ce8d11826a40354352c
Lucas <stick.goal@163.com>
2018-03-20 15:29:49 +0800

    增加mybatis-generator的示例项目

---
+++ framework/mybatis/mbg/src/main/java/me/maiz/trainning/framework/mybatis/App.java
@@ -0,0 +1,13 @@
+package me.maiz.trainning.framework.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
