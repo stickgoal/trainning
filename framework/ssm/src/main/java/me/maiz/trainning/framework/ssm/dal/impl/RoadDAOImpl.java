b292e1f6eba3795ad1867db91b89e17e5cdc827d
Lucas <stick.goal@163.com>
2018-03-26 16:22:04 +0800

    增加oracle使用序列自增组件的方式

--- framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/dal/impl/RoadDAOImpl.java
+++ framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/dal/impl/RoadDAOImpl.java
@@ -1,7 +1,9 @@
 package me.maiz.trainning.framework.ssm.dal.impl;
 
 import me.maiz.trainning.framework.ssm.dal.RoadDAO;
+import me.maiz.trainning.framework.ssm.dal.mapper.BlogMapper;
 import me.maiz.trainning.framework.ssm.dal.mapper.RoadMapper;
+import me.maiz.trainning.framework.ssm.dal.model.Blog;
 import me.maiz.trainning.framework.ssm.dal.model.Road;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,9 +14,19 @@ public class RoadDAOImpl implements RoadDAO {
 	@Autowired
 	private RoadMapper roadMapper;
 
+	@Autowired
+	private BlogMapper blogMapper;
+
 	@Override
 	public void save(Road road) {
 		roadMapper.insert(road);
+
+		Blog b = new Blog();
+		b.setState("A");
+		b.setAuthorName("A");
+		b.setFeatured((short)1);
+		b.setTitle("A");
+		blogMapper.insert(b);
 	}
 	
 
--- framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/web/RoadController.java
+++ framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/web/RoadController.java
@@ -1,13 +1,10 @@
 package me.maiz.trainning.framework.ssm.web;
 
+import me.maiz.trainning.framework.ssm.dal.model.Road;
+import me.maiz.trainning.framework.ssm.service.RoadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import me.maiz.trainning.framework.ssm.dal.model.Road;
-import me.maiz.trainning.framework.ssm.service.RoadService;
-
-import java.math.BigDecimal;
 
 @Controller
 public class RoadController {
@@ -18,9 +15,11 @@ public class RoadController {
 	@RequestMapping("addRoad")
 	public String add(String roadName){
 		Road road = new Road();
-		road.setRoadId(new BigDecimal(11));
 		road.setRoadName(roadName);
 		roadService.register(road);
+
+
+
 		return "addSuccess";
 	}
 	
