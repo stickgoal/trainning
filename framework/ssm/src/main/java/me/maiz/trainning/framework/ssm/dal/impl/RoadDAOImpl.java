0da3d494ecdc6d6342a30dd8ece572a83c8b9a86
Lucas <stick.goal@163.com>
2018-03-28 16:17:38 +0800

    添加mybatis-generator的plugin以及svn忽略无用文件的方式

--- framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/dal/impl/RoadDAOImpl.java
+++ framework/ssm/src/main/java/me/maiz/trainning/framework/ssm/dal/impl/RoadDAOImpl.java
@@ -5,6 +5,8 @@ import me.maiz.trainning.framework.ssm.dal.mapper.BlogMapper;
 import me.maiz.trainning.framework.ssm.dal.mapper.RoadMapper;
 import me.maiz.trainning.framework.ssm.dal.model.Blog;
 import me.maiz.trainning.framework.ssm.dal.model.Road;
+import me.maiz.trainning.framework.ssm.dal.model.RoadExample;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -27,6 +29,9 @@ public class RoadDAOImpl implements RoadDAO {
 		b.setFeatured((short)1);
 		b.setTitle("A");
 		blogMapper.insert(b);
+
+
+		roadMapper.selectByExampleWithRowbounds(new RoadExample(),new RowBounds(0,5));
 	}
 	
 
--- vcs/svn/3-svn-manner.md
+++ vcs/svn/3-svn-manner.md
@@ -9,3 +9,17 @@
   - 若是本地运行有问题，则解决问题后，再提交。否则合作者更新你的提交后无法继续工作
 - 冲突发生时需要分析和交流，不可以直接覆盖他人提交，涉及修改他人代码则与对方直接交流，商讨过后再确定如何解决
 - 提交**必须**写message，概括本次提交的主要内容，比如，“添加了用户注册功能”；或者“修改用户重复注册的bug”，做到见message知提交内容。强烈反对写：“新增UserService.java,UserDAO.java”这类废话
+
+
+
+- 为避免本地数据冲突，需要做如下配置：
+
+  - 在任意目录右击，选择`tortoiseSVN` > `settings` >`general`>`Global ignore pattern`中添加如下内容：
+
+    ```
+    velocity.log* bin target .settings .classpath .project *.o *.lo *.la *.al .libs *.so *.so.[0-9]* *.a *.pyc *.pyo *.rej *~ #*# .#* .*.swp .DS_Store .springBeans .idea *.iml
+    ```
+
+  - 在服务器端删除pom.xml和src外的其他生成代码，比如eclipse的.classpath 和 .project文件等
+
+  - 客户端从eclipse中删除项目，更新后重新导入到eclipse中
