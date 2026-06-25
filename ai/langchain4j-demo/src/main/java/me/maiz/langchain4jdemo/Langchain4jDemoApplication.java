7a919d1bf63bf2418ee884050157078efb80d1cb
Lucas <stick.goal@163.com>
2026-06-25 11:37:40 +0800

    增加不同的ai提供商

--- ai/langchain4j-demo/src/main/java/me/maiz/langchain4jdemo/Langchain4jDemoApplication.java
+++ ai/langchain4j-demo/src/main/java/me/maiz/langchain4jdemo/Langchain4jDemoApplication.java
@@ -2,6 +2,7 @@ package me.maiz.langchain4jdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Langchain4jDemoApplication {
